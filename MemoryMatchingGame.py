36c1b84ea1104825c5379755d9de36d9ff407424
tuo50680 <tuo50680@temple.edu>
2024-11-18 21:10:37 -0500

    Update MemoryMatchingGame.py

--- MemoryMatchingGame.py
+++ MemoryMatchingGame.py
@@ -1,8 +1,22 @@
 import random
 #hello
 #test
+import pygame
+
+SCREENWIDTH = 800
+SCREENHEIGHT = 600
+screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+pygame.display.set_caption("Final Project")
 
 
 
+run = True
+while run:
+    screen.fill("pink")
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
-testestestest
+    pygame.display.update()
+
+pygame.quit()
